689bcd690af556db0f5d3f11e9ecc2362a6c7bcd
Fanny Lemaitre-Hermenier <fanny@Samsung.home>
2021-05-28 15:47:45 +0200

    challenge 12

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Lien vidéo pour challenge 10 : https://www.loom.com/share/a601c7932f2048cd992d80a89eaab056
 Lien vidéo pour challenge 11 : https://www.loom.com/share/c813c7d939804a7eb1274e95932d2d75
+Lien vidéo pour challenge 12 : https://www.loom.com/share/c434a440985642b1a423a8ca068b81fa
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+use App\Form\ProgramType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -30,6 +32,38 @@ class ProgramController extends AbstractController
         return $this->render('program/index.html.twig', [
             'programs' => $programs,
          ]);
+    }
+
+    /**
+     * The controller for the program add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Program Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to programs list
+            return $this->redirectToRoute('program_index');
+            // And redirect to a route that display the result
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
     }
 
     /**
